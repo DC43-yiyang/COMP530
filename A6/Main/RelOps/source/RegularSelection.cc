05735a98e569600dc5f384261e4193ea59d19625
Yiyang Clear <yn27@rice.com>
2023-03-28 00:37:32 -0500

    elimate the note

--- A6/Main/RelOps/source/RegularSelection.cc
+++ A6/Main/RelOps/source/RegularSelection.cc
@@ -13,43 +13,6 @@ RegularSelection :: RegularSelection (MyDB_TableReaderWriterPtr inputIn, MyDB_Ta
 
 }
 
-// void RegularSelection :: run () {
-//     // Get the input record
-//     MyDB_RecordPtr inputRec = input -> getEmptyRecord();
-
-//     // Now get the predicate
-//     // The predicate must be true for record in order to be stored into ouput table
-//     func predicate = inputRec -> compileComputation(selectionPredicate);
-
-//     // Get the final set of computations that we will used to build the output record
-//     vector <func> finalComputations;
-//     for(string s : projections){
-//         finalComputations.push_back(inputRec -> compileComputation(s));
-//     }
-
-//     // Create a ouput record
-//     MyDB_RecordPtr outputRec = output -> getEmptyRecord();
-
-//     // Now we iterate through the input record
-//     MyDB_RecordIteratorAltPtr myIter = input -> getIteratorAlt();
-
-//     while(myIter -> advance()){
-//         myIter -> getCurrent(inputRec);
-
-//         // Check to see if it is accept by the predicate
-//         if(predicate() -> toBool()){
-//             // run all computations
-//             int i = 0;
-//             for(auto &f : finalComputations){
-//                 outputRec -> getAtt(i++) -> set(f());
-//             }
-
-//             // Now we add outputRec to the output table
-//             outputRec -> recordContentHasChanged();
-//             output -> append(outputRec);
-//         }
-//     }
-// }
 void RegularSelection::run() {
     // Compile the selection predicate
     MyDB_RecordPtr inputRec = input->getEmptyRecord();
