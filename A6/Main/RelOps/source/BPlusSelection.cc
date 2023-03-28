7cc9353647aa11a9cb04ee775b5251cddd50fcb7
Yiyang Niu <yn27@jade.clear.rice.edu>
2023-03-28 00:25:14 -0500

    BPlusSelection implement

--- A6/Main/RelOps/source/BPlusSelection.cc
+++ A6/Main/RelOps/source/BPlusSelection.cc
@@ -16,6 +16,41 @@ BPlusSelection :: BPlusSelection (MyDB_BPlusTreeReaderWriterPtr inputIn, MyDB_Ta
 
 }
 
-void BPlusSelection :: run () {}
+void BPlusSelection::run() {
+    // Compile the selection predicate
+    MyDB_RecordPtr inputRec = input->getEmptyRecord();
+    func predicateFunc = inputRec->compileComputation(selectionPredicate);
 
+    // Compile the projection functions
+    vector<func> projectionFuncs;
+    for (auto &proj : projections) {
+        projectionFuncs.push_back(inputRec->compileComputation(proj));
+    }
+
+    // Initialize the output record
+    MyDB_RecordPtr outputRec = output->getEmptyRecord();
+
+    // Search for records in the B+ tree with keys between low and high (inclusive)
+    MyDB_RecordIteratorAltPtr myIter = input->getRangeIteratorAlt(low, high);
+
+    // Iterate through the records
+    while (myIter->advance()) {
+        myIter-> getCurrent(inputRec);
+
+        // Check if the record satisfies the selection predicate
+        if (!predicateFunc()->toBool()) {
+            continue;
+        }
+
+        // Apply the projection functions to create the output record
+        int i = 0;
+        for (auto &projFunc : projectionFuncs) {
+            outputRec->getAtt(i++)->set(projFunc());
+        }
+        outputRec->recordContentHasChanged();
+
+        // Append the output record to the output table
+        output->append(outputRec);
+    }
+}
 #endif
